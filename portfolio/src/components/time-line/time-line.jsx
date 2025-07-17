cc375677e9196bfee8944a92df169dc40f96d823
lbercovitch <leah.bercovitch@gmail.com>
2025-07-16 22:35:25 -0700

    updating font

--- portfolio/src/components/time-line/time-line.jsx
+++ portfolio/src/components/time-line/time-line.jsx
@@ -7,11 +7,7 @@ function TimeLine() {
     <div className="font-josefin-sans text-gray-800 text-xl md:ml-12 md:mb-24 md:pl-20 md:border-l-5 md:border-[#333]">
       {resumeItems.map(({ id, dates, organization, projects }) => (
         <div key={id}>
-          <h2 className="w-full mt-8 text-center
-            font-super-carnival text-transparent text-3xl sm:text-5xl
-            text-stroke-color-[#333] text-stroke-width-[2px]
-            text-shadow-[2px_2px_#6acbf8] sm:text-shadow-[3px_3px_#6acbf8]"
-          >
+          <h2 className="w-full mt-8 text-center font-josefin-sans-bold text-3xl md:text-5xl">
             {organization}
           </h2>
           <h3 className="font-josefin-sans-bold text-center text-3xl mb-6">
@@ -19,7 +15,7 @@ function TimeLine() {
           </h3>
           {projects.map((project) => (
             <div key={project.id} className="relative mb-12 border-2 border-[#333] bg-sky-100 rounded-2xl p-10">
-              <span className="hidden md:block absolute left-[-98px] top-20 w-8 h-8 bg-sky-400 border-4 border-[#333] rounded-full shadow-md" />
+              <span className="hidden md:block absolute left-[-98px] top-20 w-8 h-8 bg-[#6acbf8] border-4 border-[#333] rounded-full shadow-md" />
               <Project project={project} />
             </div>
           ))}
