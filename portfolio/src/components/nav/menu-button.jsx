f4653c7acd2db5d64e5656f7e2994d53d440ee44
lbercovitch <leah.bercovitch@gmail.com>
2025-07-21 16:46:46 -0700

    fixing typo

--- portfolio/src/components/nav/menu-button.jsx
+++ portfolio/src/components/nav/menu-button.jsx
@@ -13,9 +13,9 @@ function MenuButton({ active, setActive }) {
       className="h-full flex flex-col items-center justify-center px-8 cursor-pointer"
       onClick={toggleActive}
     >
-      <div class={`bar1 ${active ? 'rotate-45 translate-y-3.5' : ''}`}></div>
-      <div class={`bar2 ${active ? 'opacity-0' : ''}`}></div>
-      <div class={`bar3 ${active ? '-rotate-45 -translate-y-3.5' : ''}`}></div>
+      <div className={`bar1 ${active ? 'rotate-45 translate-y-3.5' : ''}`}></div>
+      <div className={`bar2 ${active ? 'opacity-0' : ''}`}></div>
+      <div className={`bar3 ${active ? '-rotate-45 -translate-y-3.5' : ''}`}></div>
     </button>
   );
 }
