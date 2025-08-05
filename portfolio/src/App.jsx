067453d24171fe176b4da0adea459e5b2700fb33
lbercovitch <leah.bercovitch@gmail.com>
2025-08-04 17:07:23 -0700

    updating node modules and using lazing importing for routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,20 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
-import Landing from './pages/landing';
-import About from './pages/about';
-import Projects from './pages/projects';
-import Contact from './pages/contact';
-import CountryQuiz from './pages/projects/country-quiz';
+import React, { lazy, Suspense } from 'react';
+
+const Landing = lazy(() => import('./pages/landing'));
+const About = lazy(() => import('./pages/about'));
+const Projects = lazy(() => import('./pages/projects'));
+const Contact = lazy(() => import('./pages/contact'));
+const CountryQuiz = lazy(() => import('./pages/projects/country-quiz'));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/projects/country-quiz" element={<CountryQuiz />} />
-    </Routes>
+    <Suspense fallback={<></>}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects/country-quiz" element={<CountryQuiz />} />
+      </Routes>
+    </Suspense>
   )
 }
 
-export default App
+export default App;
